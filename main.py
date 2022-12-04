aeb9cd15acb0c679301ed7ae7084ffb5e654fe7b
Luna <49705655+Luunae@users.noreply.github.com>
2022-12-04 11:36:51 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Advent of Code 2022, Luna L Nova, assisted by Github Copilot.
+# The first part of this file is code to run each part of each day's challenges.
+# The second part of this file will be any helper functions needed on multiple days.
+# Puzzle inputs will be found in /inputs
+
+# ====================== Daily Challenges ======================
+
+
+# ====================== Helper Functions ======================
